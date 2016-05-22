eb963608c939bce46d0b6399fc0d3d779f65a284
Fred Rivett <fred@fredrivett.com>
2016-05-22 20:56:43 +0100

    Update LTL details

--- partials/project/details/ltl.php
+++ partials/project/details/ltl.php
@@ -3,14 +3,13 @@
     <div class="small-12 medium-8 columns">
       <div class="c_project-detail__main">
         <h2><a href="https://learningtolaunch.co/">Learning To Launch <i class="fa fa-external-link"></i></a></h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.</p>
+        <p>Learning To Launch is a short ebook where we share what we learnt launching six projects in six months.</p>
 
         <h3>Related links</h3>
         <ul class="o_undercover-list c_project-detail__related">
           <li><a href="https://learningtolaunch.co/">LearingToLaunch.co</a></li>
           <li><a href="https://learningtolaunch.co/read">Read online for free</a></li>
-          <li><a href="#">{{ How we wrote a book but I dont even know why how what even }}</a></li>
-          <li><a href="#">Product Hunt Launch</a></li>
+          <li><a href="https://www.producthunt.com/books/learning-to-launch">Product Hunt Launch</a></li>
         </ul>
       </div>
     </div>
@@ -19,9 +18,9 @@
         <h4>Project Type</h4>
         <span>Book</span>
         <h4>Date launched</h4>
-        <span>Dec 2016</span>
+        <span>Mar 2016</span>
         <h4>Time invested</h4>
-        <span>{{X}} hours</span>
+        <span>236 hours</span>
         <h4>Status</h4>
         <span>Live</span>
       </div>
