e0f97b196cd1847cffccf429c131d8bde6545f88
Fred Rivett <fred@fredrivett.com>
2016-08-08 11:17:53 +0100

    Adds privacy policy page + splits head & foot into partials
    * Adds new privacy policy page reworked from the same one we use for
       everything.
    * Splits head and foot off into partials now we have more than one page.
    * Adds t_homepage theme to make text big on homepage but not by
       standard.

--- index.php
+++ index.php
@@ -1,205 +1,143 @@
-<!doctype html>
-<html class="no-js" lang="en">
-  <head>
-    <meta charset="utf-8">
+<body class="t_homepage">
 
-    <!-- Always force latest IE rendering engine or request Chrome Frame -->
-    <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">
+  <div class="u_hidden">
+    <?php require("partials/svg-defs.php"); ?>
+  </div>
 
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <title>We Are CONTRAST</title>
-
-    <link href="css/app.css" rel="stylesheet">
-    <link href='https://fonts.googleapis.com/css?family=Lato:900,300,300italic,900italic' rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-
-    <link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-114x114.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="144x144"  href="/apple-touch-icon-144x144.png">
-    <link rel="apple-touch-icon" sizes="60x60" href="/apple-touch-icon-60x60.png">
-    <link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon-120x120.png">
-    <link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon-76x76.png">
-    <link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-152x152.png">
-    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-180x180.png">
-    <link rel="icon" type="image/png" href="/favicon-192x192.png" sizes="192x192">
-    <link rel="icon" type="image/png" href="/favicon-160x160.png" sizes="160x160">
-    <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96">
-    <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
-    <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
-    <meta name="msapplication-TileColor" content="#000000">
-    <meta name="msapplication-TileImage" content="/mstile-144x144.png">
-    <meta name="p:domain_verify" content="ebca3e22b99bba1cf14889cc51a2b10d"/>
-  </head>
-
-  <body>
-    <div class="u_hidden">
-      <?php require("partials/svg-defs.php"); ?>
-    </div>
-
-    <div class="c_bar">
-      <form action="https://www.getdrip.com/forms/7565551/submissions" method="post" data-drip-embedded-form="7565551">
-        <div class="o_spacer o_spacer--small">
-          <label for="email">Get our weekly startup email&nbsp;direct&nbsp;to&nbsp;your&nbsp;inbox</label>
-          <div class="o_h-form-flex c_signup-form">
-            <div class="o_h-form-flex__input c_signup-form__input">
-              <input type="email" name="fields[email]" value="" class="c_input-button__input" placeholder="Your email address" />
-            </div>
-            <div class="o_h-form-flex__btn">
-              <input type="submit" name="submit" value="Sign me up" class="c_button" data-drip-attribute="sign-up-button" />
-            </div>
+  <div class="c_bar">
+    <form action="https://www.getdrip.com/forms/7565551/submissions" method="post" data-drip-embedded-form="7565551">
+      <div class="o_spacer o_spacer--small">
+        <label for="email">Get our weekly startup email&nbsp;direct&nbsp;to&nbsp;your&nbsp;inbox</label>
+        <div class="o_h-form-flex c_signup-form">
+          <div class="o_h-form-flex__input c_signup-form__input">
+            <input type="email" name="fields[email]" value="" class="c_input-button__input" placeholder="Your email address" />
+          </div>
+          <div class="o_h-form-flex__btn">
+            <input type="submit" name="submit" value="Sign me up" class="c_button" data-drip-attribute="sign-up-button" />
           </div>
         </div>
-      </form>
-    </div>
-
-    <div class="c_header-thing text-center">
-      <div class="row">
-        <div class="small-12 columns">
-          <h1 class="o_block o_block--pt-0 c_logo">
-            <span class="c_logo__preface">We Are</span>
-            <svg viewBox="0 0 342.125 48" x="0px" y="0px" width="342.125px" height="48px" class="c_logo__mark c_main-nav__logo">
-              <title>
-                Contrast
-              </title>
-              <use xlink:href="#logo"></use>
-            </svg>
-          </h1>
-
-          Hey there. We're <a href="https://twitter.com/mikeaag" target="_blank" class="o_cta">Mike</a> &amp; <a href="https://twitter.com/fredrivett" target="_blank" class="o_cta">Fred</a>, and we are Contrast. We <a href="https://blog.wearecontrast.com/" class="o_cta">write</a> &amp; <a href="#things-weve-made" class="o_cta">make stuff</a>.
-        </div>
       </div>
-    </div>
+    </form>
+  </div>
 
-    <div class="o_overlap">
-      <div class="row">
-        <div class="small-12 columns text-center">
-          <ul class="o_horizontal-list c_social-list o_horizontal-list--centred">
-            <li>
-              <a href="https://twitter.com/wecontrast" target="_blank">
-                <svg viewBox="0 0 200 162.5" x="0px" y="0px" width="200px" height="162.5px" class="c_social-list__image c_social-list__image--twitter">
-                  <title>
-                    Twitter
-                  </title>
-                  <use xlink:href="#twitter"></use>
-                </svg>
-              </a>
-            </li>
-            <li>
-              <a href="https://blog.wearecontrast.com/" target="_blank">
-                <div class="c_social-list__image c_social-list__image--medium" aria-label="Medium"></div>
-              </a>
-            </li>
-            <li>
-              <a href="https://www.producthunt.com/@mikeaag/collections/i-made-that" target="_blank">
-                <svg viewBox="0 0 240 240" x="0px" y="0px" width="240px" height="240px" class="c_social-list__image c_social-list__image--ph">
-                  <title>
-                    Product Hunt
-                  </title>
-                  <use xlink:href="#ph"></use>
-                </svg>
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
-
-    <div class="o_block o_block--large" id="things-weve-made">
-      <div class="row">
-        <div class="small-12 small-centered columns">
-          <h2 class="text-center">Things we've made</h2>
-
-          <ul class="o_undercover-list c_project-list js_project-list">
-            <li>
-              <a href="#" class="js_project-link">Real Time Users</a>
-              <?php require("partials/project/details/rtu.php") ?>
-            </li>
-            <li>
-              <a href="#" class="js_project-link">Learning To Launch</a>
-              <?php require("partials/project/details/ltl.php") ?>
-            </li>
-            <li>
-              <a href="#" class="js_project-link">MyYear</a>
-              <?php require("partials/project/details/my.php") ?>
-            </li>
-            <li>
-              <a href="#" class="js_project-link">TheWorkingLunch</a>
-              <?php require("partials/project/details/twl.php") ?>
-            </li>
-            <li>
-              <a href="#" class="js_project-link">FormFillerJS</a>
-              <?php require("partials/project/details/ff.php") ?>
-            </li>
-            <li>
-              <a href="#" class="js_project-link">FoundersKit</a>
-              <?php require("partials/project/details/fk.php") ?>
-            </li>
-            <li>
-              <a href="#" class="js_project-link">Outstanding Bar</a>
-              <?php require("partials/project/details/ob.php") ?>
-            </li>
-            <li>
-              <a href="#" class="js_project-link">FlashTabs</a>
-              <?php require("partials/project/details/ft.php") ?>
-            </li>
-            <li>
-              <a href="#" class="js_project-link">HowsItGoin</a>
-              <?php require("partials/project/details/hig.php") ?>
-            </li>
-            <li>
-              <a href="#" class="js_project-link">AreTheNsaWatchingMe</a>
-              <?php require("partials/project/details/nsa.php") ?>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
-
+  <div class="c_header-thing text-center">
     <div class="row">
-      <div class="small-12 small-centered columns text-center">
-        <footer class="o_block o_block--pt-0 c_main-footer">
-          <a href="https://blog.wearecontrast.com/we-are-contrast-e0c511883c1f#.f13nzkqg6" target="_blank" class="c_main-footer__link">Since 2014</a>
-        </footer>
+      <div class="small-12 columns">
+        <h1 class="o_block o_block--pt-0 c_logo">
+          <span class="c_logo__preface">We Are</span>
+          <svg viewBox="0 0 342.125 48" x="0px" y="0px" width="342.125px" height="48px" class="c_logo__mark c_main-nav__logo">
+            <title>
+              Contrast
+            </title>
+            <use xlink:href="#logo"></use>
+          </svg>
+        </h1>
+
+        Hey there. We're <a href="https://twitter.com/mikeaag" target="_blank" class="o_cta">Mike</a> &amp; <a href="https://twitter.com/fredrivett" target="_blank" class="o_cta">Fred</a>, and we are Contrast. We <a href="https://blog.wearecontrast.com/" class="o_cta">write</a> &amp; <a href="#things-weve-made" class="o_cta">make stuff</a>.
       </div>
     </div>
+  </div>
 
+  <div class="o_overlap">
     <div class="row">
-      <div class="small-12 small-centered columns text-center">
-        <footer class="o_block o_block--pt-0 c_main-footer">
-          <div class="realtimeuserscounter realtimeuserscounter--styled"></div>
-          <script src="https://realtimeusers.bycontrast.co/realtimeusers.js"></script>
-        </footer>
+      <div class="small-12 columns text-center">
+        <ul class="o_horizontal-list c_social-list o_horizontal-list--centred">
+          <li>
+            <a href="https://twitter.com/wecontrast" target="_blank">
+              <svg viewBox="0 0 200 162.5" x="0px" y="0px" width="200px" height="162.5px" class="c_social-list__image c_social-list__image--twitter">
+                <title>
+                  Twitter
+                </title>
+                <use xlink:href="#twitter"></use>
+              </svg>
+            </a>
+          </li>
+          <li>
+            <a href="https://blog.wearecontrast.com/" target="_blank">
+              <div class="c_social-list__image c_social-list__image--medium" aria-label="Medium"></div>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.producthunt.com/@mikeaag/collections/i-made-that" target="_blank">
+              <svg viewBox="0 0 240 240" x="0px" y="0px" width="240px" height="240px" class="c_social-list__image c_social-list__image--ph">
+                <title>
+                  Product Hunt
+                </title>
+                <use xlink:href="#ph"></use>
+              </svg>
+            </a>
+          </li>
+        </ul>
       </div>
     </div>
+  </div>
 
+  <div class="o_block o_block--large" id="things-weve-made">
+    <div class="row">
+      <div class="small-12 small-centered columns">
+        <h2 class="text-center">Things we've made</h2>
 
+        <ul class="o_undercover-list c_project-list js_project-list">
+          <li>
+            <a href="#" class="js_project-link">Real Time Users</a>
+            <?php require("partials/project/details/rtu.php") ?>
+          </li>
+          <li>
+            <a href="#" class="js_project-link">Learning To Launch</a>
+            <?php require("partials/project/details/ltl.php") ?>
+          </li>
+          <li>
+            <a href="#" class="js_project-link">MyYear</a>
+            <?php require("partials/project/details/my.php") ?>
+          </li>
+          <li>
+            <a href="#" class="js_project-link">TheWorkingLunch</a>
+            <?php require("partials/project/details/twl.php") ?>
+          </li>
+          <li>
+            <a href="#" class="js_project-link">FormFillerJS</a>
+            <?php require("partials/project/details/ff.php") ?>
+          </li>
+          <li>
+            <a href="#" class="js_project-link">FoundersKit</a>
+            <?php require("partials/project/details/fk.php") ?>
+          </li>
+          <li>
+            <a href="#" class="js_project-link">Outstanding Bar</a>
+            <?php require("partials/project/details/ob.php") ?>
+          </li>
+          <li>
+            <a href="#" class="js_project-link">FlashTabs</a>
+            <?php require("partials/project/details/ft.php") ?>
+          </li>
+          <li>
+            <a href="#" class="js_project-link">HowsItGoin</a>
+            <?php require("partials/project/details/hig.php") ?>
+          </li>
+          <li>
+            <a href="#" class="js_project-link">AreTheNsaWatchingMe</a>
+            <?php require("partials/project/details/nsa.php") ?>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
 
-    <script src="bower_components/jquery/dist/jquery.js"></script>
-    <script src="bower_components/what-input/what-input.js"></script>
-    <script src="bower_components/foundation-sites/dist/foundation.js"></script>
-    <script src="js/all.js"></script>
+  <div class="row">
+    <div class="small-12 small-centered columns text-center">
+      <footer class="o_block o_block--pt-0 c_main-footer">
+        <a href="https://blog.wearecontrast.com/we-are-contrast-e0c511883c1f#.f13nzkqg6" target="_blank" class="c_main-footer__link">Since 2014</a>
+      </footer>
+    </div>
+  </div>
 
-    <script type="text/javascript">
-      if (document.location.hostname == "wearecontrast.com") {
-        (function (i, s, o, g, r, a, m) {
-          i['GoogleAnalyticsObject'] = r;
-          i[r] = i[r] || function () {
-            (i[r].q = i[r].q || []).push(arguments)
-          }, i[r].l = 1 * new Date();
-          a = s.createElement(o),
-                  m = s.getElementsByTagName(o)[0];
-          a.async = 1;
-          a.src = g;
-          m.parentNode.insertBefore(a, m)
-        })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+  <div class="row">
+    <div class="small-12 small-centered columns text-center">
+      <footer class="o_block o_block--pt-0 c_main-footer">
+        <div class="realtimeuserscounter realtimeuserscounter--styled"></div>
+        <script src="https://realtimeusers.bycontrast.co/realtimeusers.js"></script>
+      </footer>
+    </div>
+  </div>
 
-        ga('create', 'UA-56010312-1', 'auto');
-        ga('require', 'linkid', 'linkid.js');
-        ga('require', 'displayfeatures');
-        ga('send', 'pageview');
-      }
-    </script>
-  </body>
-</html>
+<?php include('partials/foot.php') ?>
---
+++ partials/foot.php
@@ -0,0 +1,28 @@
+  <script src="bower_components/jquery/dist/jquery.js"></script>
+  <script src="bower_components/what-input/what-input.js"></script>
+  <script src="bower_components/foundation-sites/dist/foundation.js"></script>
+  <script src="js/all.js"></script>
+
+  <script type="text/javascript">
+    if (document.location.hostname == "wearecontrast.com") {
+      (function (i, s, o, g, r, a, m) {
+        i['GoogleAnalyticsObject'] = r;
+        i[r] = i[r] || function () {
+          (i[r].q = i[r].q || []).push(arguments)
+        }, i[r].l = 1 * new Date();
+        a = s.createElement(o),
+                m = s.getElementsByTagName(o)[0];
+        a.async = 1;
+        a.src = g;
+        m.parentNode.insertBefore(a, m)
+      })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+      ga('create', 'UA-56010312-1', 'auto');
+      ga('require', 'linkid', 'linkid.js');
+      ga('require', 'displayfeatures');
+      ga('send', 'pageview');
+    }
+  </script>
+
+</body>
+</html>
