ecd14dba201bb69f0fd0592e04494df2c441d1c1
Fred Rivett <fred@fredrivett.com>
2017-08-22 21:51:03 +0100

    Add UserCompass PH Upcoming widget to site

--- partials/foot.php
+++ partials/foot.php
@@ -24,5 +24,20 @@
     }
   </script>
 
+  <script>
+    window.productHuntUpcoming = {
+      appId: 201,
+      position: 'bottomLeft',
+    };
+
+    (function(doc, scr, src, a, b) {
+      a = doc.createElement(scr);
+      b = doc.getElementsByTagName(scr)[0];
+      a.async = true;
+      a.src = src;
+      b.parentNode.insertBefore(a, b);
+    })(document, 'script', 'https://assets.producthunt.com/assets/upwigloader.js');
+  </script>
+
 </body>
 </html>
